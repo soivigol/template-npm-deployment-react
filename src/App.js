b5e0c98e6f82623dfba8b87291af4d6a601ab2f7
David Viña <david@davidvina.es>
2025-01-06 11:04:24 +0100

    add compilation in GitHub action

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskList from './components/TaskList';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <TaskList />
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+
+// tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{js,jsx,html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
